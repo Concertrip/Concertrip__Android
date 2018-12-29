2a6323962fe05b8cdd986711b7f8746ab9301bbb
cchloe2311 <cchloe2311@daum.net>
2018-12-29 21:31:07 +0900

    2018/12/29 fixed Concert to data class

--- app/src/main/java/concertrip/sopt/com/concertrip/model/Concert.kt
+++ app/src/main/java/concertrip/sopt/com/concertrip/model/Concert.kt
@@ -3,26 +3,33 @@ package concertrip.sopt.com.concertrip.model
 import concertrip.sopt.com.concertrip.interfaces.ListData
 import concertrip.sopt.com.concertrip.utillity.Constants
 
-class Concert : ListData {
+data class Concert(
 
-    var idx: Int=0
-    var title : String = ""
-    var profileImg : String ="https://img.huffingtonpost.com/asset/5ba482b82400003100546bc3.jpeg"
-    var backImg : String ="https://img.huffingtonpost.com/asset/5ba482b82400003100546bc3.jpeg"
-    var location : String =""
-    var station : String =""
-    var genre : String =""
-    var cast : String =""
-    var date : String =""
-    var price : String =""
-    var youtubeUrl : String =""
-    var subscribeNum : Int = 0
+    var idx: Int,
+    var title : String,
+    var profileImg : String,
+    var backImg : String?,
+    var location : String?,
+    var station : String?,
+    var genre : String?,
+    var date : String?,
+    var price : String?,
+    var youtubeUrl : String?,
+    var subscribeNum : Int,
+    var artistList : ArrayList<Artist>
 
-    var artistList : ArrayList<Artist> = ArrayList()
+) : ListData{
+    constructor(idx : Int, title : String, profileImg : String)
+            : this(idx, title, profileImg, null, null, null, null,
+        null, null, null, 0, ArrayList())
+
+    constructor(idx : Int)
+            : this(idx, "힙합 페스티벌", "https://search.pstatic.net/common?type=a&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2Fportrait%2F201801%2F20180108113919887.jpg",
+        "https://img.huffingtonpost.com/asset/5ba482b82400003100546bc3.jpeg", null, null, null,
+        "2018-12-23", null, null, 0, ArrayList())
+
 
     fun getTag() : String ="#$genre #$genre"
-
-
 
     override fun getType(): Int = Constants.TYPE_CONCERT
 
@@ -39,15 +46,7 @@ class Concert : ListData {
         fun getDummyArray() : ArrayList<Concert>{
             val list = ArrayList<Concert>()
             for(i in 0..5) {
-                val c = Concert()
-                c.title="힙합 페스티발"
-                c.idx=i
-                c.genre="#힙합"
-                c.youtubeUrl=""
-                c.backImg="https://img.huffingtonpost.com/asset/5ba482b82400003100546bc3.jpeg"
-                c.profileImg="https://search.pstatic.net/common?type=a&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2Fportrait%2F201801%2F20180108113919887.jpg"
-                c.cast="지코"
-                c.date="2018-12-23"
+                val c = Concert(i)
                 list.add(c)
             }
             return list
