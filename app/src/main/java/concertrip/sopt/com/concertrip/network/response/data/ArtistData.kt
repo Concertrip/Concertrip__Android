bfdaf547c2dfb3aec24c8c387a3e63021911c795
cchloe2311 <cchloe2311@daum.net>
2018-12-30 15:35:36 +0900

    2018/12/30 fixed ArtistActivity.kt as ConcertActivity.kt

--- app/src/main/java/concertrip/sopt/com/concertrip/network/response/data/ArtistData.kt
+++ app/src/main/java/concertrip/sopt/com/concertrip/network/response/data/ArtistData.kt
@@ -1,6 +1,7 @@
 package concertrip.sopt.com.concertrip.network.response.data
 
 import concertrip.sopt.com.concertrip.model.Artist
+import concertrip.sopt.com.concertrip.model.Concert
 
 data class ArtistData(
     var _id : String,
@@ -11,5 +12,21 @@ data class ArtistData(
     var tag : String,
     var youtubeUrl : String
 ){
-    fun toArtist(): Artist = Artist(_id = _id,profileImg = profileImg,backImg = backImg,name = name, genre = "" ,youtubeUrl = youtubeUrl,subscribeNum = subscribeNum)
+    //fun toArtist(): Artist = Artist(_id = _id,profileImg = profileImg,backImg = backImg,name = name, genre = "" ,youtubeUrl = youtubeUrl,subscribeNum = subscribeNum)
+    fun toArtist() : Artist {
+        val a =  Artist(_id = _id)
+        a.name = name
+        a.profileImg =  profileImg
+        a.backImg = backImg
+        a.tag = tag
+        a.youtubeUrl = youtubeUrl
+
+//        val list = ArrayList<Concert>()
+//        concertList.forEach{
+//            list.add(it.toConcert())
+//        }
+//        c.concertList = list
+
+        return a
+    }
 }
