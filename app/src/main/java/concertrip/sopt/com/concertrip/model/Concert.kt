4fe88e670c56422772fbda321107c7edd63f124d
gds04070 <gds04070@gmail.com>
2018-12-30 21:13:25 +0900

    2018/12/30 DummyData+CalendarView 수정

--- app/src/main/java/concertrip/sopt/com/concertrip/model/Concert.kt
+++ app/src/main/java/concertrip/sopt/com/concertrip/model/Concert.kt
@@ -57,17 +57,17 @@ data class Concert(
 
         }
 
-        fun getDummy(_id : String)= Concert(_id = _id, title = "힙합 페스티발", genre = "힙합", youtubeUrl = "",backImg = "https://img.huffingtonpost.com/asset/5ba482b82400003100546bc3.jpeg",
+        fun getDummy(_id : String)= Concert(_id = _id, title = "힙합 페스티발", genre = "힙합", youtubeUrl = "https://www.youtube.com/watch?v=Vl1kO9hObpA",backImg = "https://img.huffingtonpost.com/asset/5ba482b82400003100546bc3.jpeg",
             profileImg = "https://search.pstatic.net/common?type=a&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2Fportrait%2F201801%2F20180108113919887.jpg"
             ,cast = "지코", date = listOf("2018-12-20", "2018-12-21"),artistList = ArrayList<Artist>(),location = "",station = "",
             seatName = listOf("R", "VIP"), seatPrice = listOf("80,000", "120,000") ,  subscribeNum = 0)
 
-        fun getDummy_1(_id : String)= Concert(_id = _id, title = "SM콘서트", genre = "보이그룹", youtubeUrl = "",backImg = "http://blogfiles.naver.net/20150115_278/jimin1226_1421248926310a8IkG_JPEG/vsbspvs.jpg",
+        fun getDummy_1(_id : String)= Concert(_id = _id, title = "SM콘서트", genre = "보이그룹", youtubeUrl = "https://www.youtube.com/watch?v=_cAskH1PtmQ",backImg = "http://blogfiles.naver.net/20150115_278/jimin1226_1421248926310a8IkG_JPEG/vsbspvs.jpg",
             profileImg = "https://search.pstatic.net/common?type=a&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2Fportrait%2F201805%2F20180530151238597-2777067.jpg"
             ,cast = "태민", date = listOf("2018-12-16", "2018-12-17"),artistList = ArrayList<Artist>(),location = "",station = "",
             seatName = listOf("플로어", "3층"), seatPrice = listOf("110,000", "120,000") ,  subscribeNum = 0)
 
-        fun getDummy_2(_id : String)= Concert(_id = _id, title = "휘성 콘서트", genre = "#발라드", youtubeUrl = "",backImg = "http://imgnews.naver.net/image/213/2016/07/20/20160720_1468977905_09715900_1_99_20160720103907.jpg",
+        fun getDummy_2(_id : String)= Concert(_id = _id, title = "휘성 콘서트", genre = "#발라드", youtubeUrl = "https://www.youtube.com/watch?v=YXiLkrSft1w",backImg = "http://imgnews.naver.net/image/213/2016/07/20/20160720_1468977905_09715900_1_99_20160720103907.jpg",
             profileImg = "https://search.pstatic.net/common?type=a&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2F185%2F201709151548017101.jpg"
             ,cast = "휘성", date = listOf("2018-12-28", "2018-12-29"),artistList = ArrayList<Artist>(),location = "",station = "",
             seatName = listOf("R", "VIP"), seatPrice = listOf("70,000", "80,000") ,  subscribeNum = 0)
--- app/src/main/java/concertrip/sopt/com/concertrip/network/response/data/ArtistData.kt
+++ app/src/main/java/concertrip/sopt/com/concertrip/network/response/data/ArtistData.kt
@@ -34,13 +34,14 @@ data class ArtistData(
             return ArtistData("",0,"","","","","")
         }
 
-              fun getDummyArray(): ArrayList<ArtistData>{
+        fun getDummyArray(): ArrayList<ArtistData>{
             val list = ArrayList<ArtistData>()
-            list.add(getDummy())
-            list.add(getDummy())
-            list.add(getDummy())
-            list.add(getDummy())
-            list.add(getDummy())
+            list.add(ArtistData("",1000, "지코", "https://search.pstatic.net/common?type=a&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2Fportrait%2F201801%2F20180108113919887.jpg",
+                "https://img.huffingtonpost.com/asset/5ba482b82400003100546bc3.jpeg", "힙합","https://www.youtube.com/watch?v=Vl1kO9hObpA"))
+            list.add(ArtistData("", 1050, "태민","https://search.pstatic.net/common?type=a&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2Fportrait%2F201805%2F20180530151238597-2777067.jpg",
+                "http://blogfiles.naver.net/20150115_278/jimin1226_1421248926310a8IkG_JPEG/vsbspvs.jpg","보이그룹","https://www.youtube.com/watch?v=_cAskH1PtmQ"))
+            list.add(ArtistData("",1100,"휘성","https://search.pstatic.net/common?type=a&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2F185%2F201709151548017101.jpg",
+                "http://imgnews.naver.net/image/213/2016/07/20/20160720_1468977905_09715900_1_99_20160720103907.jpg","발라드","https://www.youtube.com/watch?v=YXiLkrSft1w"))
             return list
         }
     }
