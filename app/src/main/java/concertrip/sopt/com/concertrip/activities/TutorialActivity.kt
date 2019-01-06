b95ea0887fa2aebf8d340d2759859ddd2b29d1c9
gds04070 <gds04070@gmail.com>
2019-01-06 16:42:48 +0900

    2019/01/06 Tutorial View

--- app/src/main/java/concertrip/sopt/com/concertrip/activities/TutorialActivity.kt
+++ app/src/main/java/concertrip/sopt/com/concertrip/activities/TutorialActivity.kt
@@ -43,7 +43,107 @@ class TutorialActivity : AppCompatActivity() {
 
         }
 
+        btn_tutorial_2.setOnClickListener{
+            clickTest[1] = 1-clickTest[1]
 
+            if(clickTest[1] == 1)
+            {iv_tutorial_click_2.visibility = View.VISIBLE
+                iv_tutorial_noclick_2.visibility = View.GONE}
+            else
+            {iv_tutorial_click_2.visibility = View.GONE
+                iv_tutorial_noclick_2.visibility =View.VISIBLE}
+
+        }
+
+
+        btn_tutorial_3.setOnClickListener{
+            clickTest[2] = 1-clickTest[2]
+
+            if(clickTest[2] == 1)
+            {iv_tutorial_click_3.visibility = View.VISIBLE
+                iv_tutorial_noclick_3.visibility = View.GONE}
+            else
+            {iv_tutorial_click_3.visibility = View.GONE
+                iv_tutorial_noclick_3.visibility =View.VISIBLE}
+
+        }
+
+        btn_tutorial_4.setOnClickListener{
+            clickTest[3] = 1-clickTest[3]
+
+            if(clickTest[3] == 1)
+            {iv_tutorial_click_4.visibility = View.VISIBLE
+                iv_tutorial_noclick_4.visibility = View.GONE}
+            else
+            {iv_tutorial_click_4.visibility = View.GONE
+                iv_tutorial_noclick_4.visibility =View.VISIBLE}
+
+        }
+
+
+        btn_tutorial_5.setOnClickListener{
+            clickTest[4] = 1-clickTest[4]
+
+            if(clickTest[4] == 1)
+            {iv_tutorial_click_5.visibility = View.VISIBLE
+                iv_tutorial_noclick_5.visibility = View.GONE}
+            else
+            {iv_tutorial_click_5.visibility = View.GONE
+                iv_tutorial_noclick_5.visibility =View.VISIBLE}
+
+        }
+
+
+        btn_tutorial_6.setOnClickListener{
+            clickTest[5] = 1-clickTest[5]
+
+            if(clickTest[5] == 1)
+            {iv_tutorial_click_6.visibility = View.VISIBLE
+                iv_tutorial_noclick_6.visibility = View.GONE}
+            else
+            {iv_tutorial_click_6.visibility = View.GONE
+                iv_tutorial_noclick_6.visibility =View.VISIBLE}
+
+        }
+
+
+        btn_tutorial_7.setOnClickListener{
+            clickTest[6] = 1-clickTest[6]
+
+            if(clickTest[6] == 1)
+            {iv_tutorial_click_7.visibility = View.VISIBLE
+                iv_tutorial_noclick_7.visibility = View.GONE}
+            else
+            {iv_tutorial_click_7.visibility = View.GONE
+                iv_tutorial_noclick_7.visibility =View.VISIBLE}
+
+        }
+
+
+        btn_tutorial_8.setOnClickListener{
+            clickTest[7] = 1-clickTest[7]
+
+            if(clickTest[7] == 1)
+            {iv_tutorial_click_8.visibility = View.VISIBLE
+                iv_tutorial_noclick_8.visibility = View.GONE}
+            else
+            {iv_tutorial_click_8.visibility = View.GONE
+                iv_tutorial_noclick_8.visibility =View.VISIBLE}
+
+        }
+
+
+        btn_tutorial_9.setOnClickListener{
+            clickTest[8] = 1-clickTest[8]
+
+            if(clickTest[8] == 1)
+            {iv_tutorial_click_9.visibility = View.VISIBLE
+                iv_tutorial_noclick_9.visibility = View.GONE}
+            else
+            {iv_tutorial_click_9.visibility = View.GONE
+                iv_tutorial_noclick_9.visibility =View.VISIBLE}
+
+        }
 
     }
 
