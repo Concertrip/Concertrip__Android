86e59a01a10fbf66ef489e7343267c078eb4ec6c
lumiere <heesung6701@naver.com>
2019-01-07 23:37:15 +0900

    2019/01/07 Toast 퉁퉁한거 방지

--- app/src/main/java/concertrip/sopt/com/concertrip/activities/main/MainActivity.kt
+++ app/src/main/java/concertrip/sopt/com/concertrip/activities/main/MainActivity.kt
@@ -7,6 +7,7 @@ import android.support.v4.app.ActivityCompat
 import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log
+import android.widget.TextView
 import android.widget.Toast
 import concertrip.sopt.com.concertrip.interfaces.OnFragmentInteractionListener
 import concertrip.sopt.com.concertrip.utillity.Constants
