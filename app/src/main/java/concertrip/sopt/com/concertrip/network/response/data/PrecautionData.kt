7fae16f9410915956b4086caff1fe9f6327b1cca
gds04070 <gds04070@gmail.com>
2019-01-10 21:24:47 +0900

    2019/01/10 padding추가

--- app/src/main/java/concertrip/sopt/com/concertrip/network/response/data/PrecautionData.kt
+++ app/src/main/java/concertrip/sopt/com/concertrip/network/response/data/PrecautionData.kt
@@ -3,9 +3,9 @@ package concertrip.sopt.com.concertrip.network.response.data
 import concertrip.sopt.com.concertrip.model.Caution
 
 data class PrecautionData(
-    var code : Int,
-    var name : String,
-    var img : String
+    var code : Int?,
+    var name : String?,
+    var img : String?
 ){
     fun toCaution() : Caution = Caution(img = img, content = name, code = code)
 
