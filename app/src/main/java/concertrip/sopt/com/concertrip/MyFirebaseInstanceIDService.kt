04cb1717a65fb7cd555d4593c4bd90d519cea7ab
gds04070 <gds04070@gmail.com>
2019-01-10 15:26:00 +0900

    2019/01/10

---
+++ app/src/main/java/concertrip/sopt/com/concertrip/MyFirebaseInstanceIDService.kt
@@ -0,0 +1,27 @@
+package concertrip.sopt.com.concertrip
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.iid.FirebaseInstanceIdService
+
+class MyFirebaseInstanceIDService : FirebaseInstanceIdService() {
+
+
+    override fun onTokenRefresh() {
+        val refreshedToken = FirebaseInstanceId.getInstance().token
+        Log.d(TAG, "Refreshed token: " + refreshedToken!!)
+        sendRegistrationToServer(refreshedToken)
+    }
+
+    private fun sendRegistrationToServer(token: String?){
+        Log.d(TAG, token!! + "")
+    }
+
+
+    companion object {
+        private val TAG = "MyFirebaseIdService"
+    }
+}
